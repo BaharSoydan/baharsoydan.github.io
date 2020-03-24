024a55205bea7a16793609a20a9974b291ad6493
Buğra Coşkun <bgra.coskn@gmail.com>
2020-03-24 18:11:40 +0300

    Starting alternative branch, derived from personal website.

---
+++ javascripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#BL").load("BotterLord.html");
+});
+
+$(document).ready(function(){
+  $("#SB").load("marchearth.github.io.html");
+});
+
+$(document).ready(function(){
+  $("#GR").load("GRAT.html");
+});
